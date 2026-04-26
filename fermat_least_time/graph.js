1e74d5da4ff30a5c4190cd661ebe056683a6aa3d
ventovulpes <icybluesquare@gmail.com>
2026-04-25 20:08:01 -0700

    fix graph scaling

--- fermat_least_time/graph.js
+++ fermat_least_time/graph.js
@@ -20,16 +20,14 @@ function drawGraph(data) {
     }
 
     // draw background
-    graphCtx.beginPath();
-    graphCtx.fillStyle = "white";
-    graphCtx.fillRect(0, 0, GRAPH_WIDTH, GRAPH_HEIGHT);
+    clearGraph();
 
     // draw points
     const maxTime = Math.min(GRAPH_MAX_TIME, Math.max(GRAPH_MIN_TIME, Math.max(...data.map(d => d.time))));
     const minTime = Math.min(...data.map(d => d.time));
     data.forEach(d => {
         const x = (d.angle + (RAY_ANGLE_SPAN / 2)) * ((GRAPH_WIDTH - 2*GRAPH_MARGIN) / RAY_ANGLE_SPAN) + GRAPH_MARGIN;
-        const y = GRAPH_HEIGHT - ((d.time - minTime) * ((GRAPH_HEIGHT - 2*GRAPH_MARGIN) / maxTime) + GRAPH_MARGIN + GRAPH_MIN_TIME_MARGIN);
+        const y = GRAPH_HEIGHT - ((d.time - minTime) * ((GRAPH_HEIGHT - 2*GRAPH_MARGIN) / (maxTime - minTime)) + GRAPH_MARGIN + GRAPH_MIN_TIME_MARGIN);
         drawPoint(graphCtx, {x: x, y: y}, GREEN, GRAPH_POINT_RADIUS);
     });
 
@@ -53,4 +51,8 @@ function drawGraph(data) {
     for (let i = 0; i <= RAY_ANGLE_SPAN; i += 30) {
         graphCtx.fillText(i - (RAY_ANGLE_SPAN / 2), GRAPH_MARGIN + i * numberSpacing, GRAPH_HEIGHT - (0.75 * GRAPH_MARGIN));
     }
+}
+
+function clearGraph() {
+    drawBackground(graphCtx, GRAPH_WIDTH, GRAPH_HEIGHT);
 }
